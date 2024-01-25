6100803caf47ae3397ac0cd6cdace51ed31d375f
Kacper Zys <kacperzys13@gmail.com>
2024-01-25 17:34:28 +0100

    js changes, dont work ofc

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const day = document.getElementsByName('day').value;
-const month = document.getElementsByName('month').value;
-const year = document.getElementsByName('year').value;
-const date = new Date(year, month, day);
+const button = document.querySelector('.main__button');
+const day = document.querySelector('#day');
+const month = document.querySelector('#month');
+const year = document.querySelector('#year');
 
-console.log(day);
+day.addEventListener('keydown', e => {
 
-let output = new Date() - date;
+    if (day.value < 1 || day.value > 31) {
+        day.classList.add('invalid');
+    }
+});
 
-console.log(output);
+month.addEventListener('keydown', e => {
+    if (month.value < 1 || month.value > 12) {
+        month.classList.add('invalid');
+    }
+});
+
+year.addEventListener('keydown', e => {
+    if (year.value > new Date().getFullYear()) {
+        year.classList.add('invalid');
+    }
+});
+
+button.addEventListener('click', e => {
+    const date = Math.abs(new Date() - new Date(year, month, day));
+
+    console.log(new Date(date));
+
+    const dayDisplay = document.querySelector('.days-display');
+    const monthDisplay = document.querySelector('.months-display');
+    const yearDisplay = document.querySelector('.years-display');
+});
